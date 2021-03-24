025fc5ced0686da5cf7fb35979c3aef0eee0af24
Maria Dukmak <62339103+MariaDukmak@users.noreply.github.com>
2021-03-24 12:13:49 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+install_requires = [
+    'flask',
+    'matplotlib',
+    'seaborn',
+    'scipy',
+    'scikit-learn',
+    'requests',
+    'numpy',
+    'pandas',
+]
+
+setup(
+    name='EmpathicRobot',
+    version='1.0.0',
+    description='EmpathicRobot opzet',
+    author='Hogeschool Utrecht AI - 2021',
+    author_email='maryadumak@gmail.com',
+    packages=[],
+    install_requires=install_requires,
+    dependency_links=[
+        # "git+https://github.com/BB8-2020/<repo_name>#egg=<repo_name>",
+    ],
+)
