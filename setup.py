fb436ac5cd33b6f6ba1258f29a4b6c3e951bcffb
Maria <62339103+XXX-MA@users.noreply.github.com>
2021-03-25 14:51:46 +0100

    Added needed files

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
-install_requires = [
-    'matplotlib',
-    'seaborn',
-    'numpy',
-    'pandas',
-]
+DEVELOPER = True
+
+install_requires = []
+
+if DEVELOPER:
+    with open('requirments-dev.txt') as file:
+        install_requires.extend(file.read().splitlines())
+
+with open('requirments.txt') as file:
+    install_requires.extend(file.read().splitlines())
+
+
 
 setup(
     name='EmpathicRobot',
@@ -13,9 +19,7 @@ setup(
     description='EmpathicRobot opzet',
     author='Hogeschool Utrecht AI - 2021',
     author_email='maryadumak@gmail.com',
-    packages=[],
+    packages=find_packages(),
     install_requires=install_requires,
-    dependency_links=[
-        # "git+https://github.com/BB8-2020/<repo_name>#egg=<repo_name>",
-    ],
+  
 )
