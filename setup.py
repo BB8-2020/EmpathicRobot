12bb76bd6adc9b7abec8922e70dbf21423906b12
Maria <62339103+XXX-MA@users.noreply.github.com>
2021-04-15 18:04:46 +0200

    Edited setup.py to add 3 requirements to the list.

--- setup.py
+++ setup.py
@@ -2,7 +2,11 @@ from setuptools import find_packages, setup
 
 DEVELOPER = True
 
-install_requires = []
+install_requires = [
+    "tensorflow",
+    "numpy",
+    "pandas",
+]
 
 if DEVELOPER:
     with open('requirments-dev.txt') as file:
@@ -12,13 +16,12 @@ with open('requirments.txt') as file:
     install_requires.extend(file.read().splitlines())
 
 
-
 setup(
     name='EmpathicRobot',
     version='1.0.0',
     description='EmpathicRobot opzet',
     author='Hogeschool Utrecht AI - 2021',
-    author_email='maryadumak@gmail.com',
+    author_email='maria.dukmak@student.hu.nl',
     packages=find_packages(),
     install_requires=install_requires,
   
