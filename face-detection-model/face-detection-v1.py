659ffb6802f298dfa19aef0aa9caf4cb4424725e
AHipWhale <charlie.choffat@gmail.com>
2021-04-16 15:10:53 +0200

    Added code that checks the input for a face
    Because:
    - The model needs to be able to check the input for a face
    
    The code now detects a face and puts a rectangle around it

--- face-detection-model/face-detection-v1.py
+++ face-detection-model/face-detection-v1.py
@@ -1,16 +1,29 @@
 """First version of the face detection model using the OpenCV library."""
-
 import cv2
 
+# load classifier
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
 # get laptop camera footage
-cap = cv2.VideoCapture(0)
+capture = cv2.VideoCapture(0)
 
 while True:
     # read and save footage
-    _, img = cap.read()
+    _, image = capture.read()
+
+    # make image gray
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # get coordinats of detected face with a scaleFactor of 2 and a minNeighbors of 4
+    face = face_cascade.detectMultiScale(gray, 2, 4)
+
+    # make one ractangle
+    if len(face) > 0:
+        (x, y, width, height) = face[0]
+        cv2.rectangle(image, (x, y), (width + x, height + y), (255, 0, 255), 2)
 
     # show footage in new window
-    cv2.imshow('Cam', img)
+    cv2.imshow('image', image)
 
     # checks which key is pressed with a 30 milisecond delay
     k = cv2.waitKey(30)
@@ -20,4 +33,4 @@ while True:
         break
 
 # closes capturing
-cap.release()
+capture.release()
