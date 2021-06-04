f6ce23f8ba420cc6d7dd81b9c4e82214abfdda38
AHipWhale <charlie.choffat@gmail.com>
2021-06-04 11:17:48 +0200

    Changed for-loop and changed comments.
    Because:
     - For-loop chose wrong face.
     - Some comments where not finished.
    
     The for-loop didn't return right face and comments were not finished.

--- face-detection-model/face-detection.py
+++ face-detection-model/face-detection.py
@@ -2,8 +2,9 @@
 import cv2
 import numpy as np
 
-# load classifier
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+# load classifier from file
+face_cascade = cv2.CascadeClassifier(r'haarcascade_frontalface_default.xml')
+
 
 def crop_to_face(image: np.ndarray, face: np.ndarray) -> np.ndarray:
     """
@@ -27,6 +28,7 @@ def crop_to_face(image: np.ndarray, face: np.ndarray) -> np.ndarray:
 
     return frame_of_face
 
+
 def reshape_image(image: np.ndarray) -> np.ndarray:
     """
     Reshapes the given image to a 48 x 48 pixels image
@@ -42,11 +44,13 @@ def reshape_image(image: np.ndarray) -> np.ndarray:
             The image reshaped to 48 x 48 pixels
     """
     reshaped_frame = cv2.resize(image, (48, 48))
+
     return reshaped_frame
+
 
 def convert_to_bytes(image: np.ndarray) -> bytes:
     """
-    Convert the given image to bytes
+    Convert the given image to bytes.
 
     Parameters
     ----------
@@ -55,7 +59,7 @@ def convert_to_bytes(image: np.ndarray) -> bytes:
     Return
     ------
         byte_im
-
+            String of bytes containing the image
     """
     # convert np.array to one-dimensional numpy array
     _, im_buf_arr = cv2.imencode(".png", image)
@@ -64,27 +68,26 @@ def convert_to_bytes(image: np.ndarray) -> bytes:
     byte_im = im_buf_arr.tobytes()
     return byte_im
 
+
 def face_from_image(img_file: str) -> bytes:
     """
-        Reads image, tries to detect faces, takes the face that is closest,
-        then scales the image down to just the face and reshapes it as 48 x 48.
-        Then it converts the image to bytes and returns the bytes.
+        Reads image, detects faces, picks closest face then crops the image to just the face, reshapes the image
+        and returns it as bytes.
 
         Parameters
         ----------
-            img: str
-                Path to image file
+            img_file: str
+                Path to image file.
 
-        Raise
-        -----
-            Error if no face can be detected
+        Raises
+        ------
+            Raises error if no face can be detected.
 
         Return
         ------
             byte_im
-                An image of a face in a 48 x 48 image in byte format
+                An image of a face in a 48 x 48 image in byte format.
     """
-
     # reads image
     image = cv2.imread(img_file)
 
@@ -98,12 +101,23 @@ def face_from_image(img_file: str) -> bytes:
     if len(detected_faces) == 0:
         raise Exception("No face was detected in the image")
 
+    # gets closest face out of the scanned faces
     closest_face = None
-    closest_face = next((face for face in detected_faces if closest_face is not None if face[-1] > closest_face[-1] and face[-2] > closest_face[-2]), detected_faces[0])
 
+    for face in detected_faces:
+        if closest_face is not None:
+            if face[-1] > closest_face[-1] and face[-2] > closest_face[-2]:
+                closest_face = face
+        else:
+            closest_face = face
+
+    # crops image to face
     frame = crop_to_face(image, closest_face)
+
+    # reshapes image to 48 x 48
     reshaped_frame = reshape_image(frame)
 
+    # convert image to string of bytes
     byte_im = convert_to_bytes(reshaped_frame)
 
     return byte_im
