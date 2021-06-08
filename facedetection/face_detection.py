61b825579a9b7187c2da4183c36071d2a965340e
AHipWhale <57904996+AHipWhale@users.noreply.github.com>
2021-06-08 14:24:33 +0200

    Face detection model (#20)
    * Added a bit of code that shows your webcam
    
    Because:
    - Beginning of the face detection model
    
    This file opens a new window that shows your webcam in it using OpenCV.
    
    * Added two libraries to requirements.txt
    
    Because:
    - To run the models you need to install these libraries
    
    The cv2 (opencv) and tensorflow libraries were added to requirements.txt
    
    * Added code that checks the input for a face
    
    Because:
    - The model needs to be able to check the input for a face
    
    The code now detects a face and puts a rectangle around it
    
    * Added code so now it returns a face in an image
    
    Because:
     - Our model needs just a face as input.
     - The camera gives us a image but we just need the face
     - The image needs to be reshaped
    
    Added a function that gets an image and returns just a face in byte format
    
    * Split code in more functions and added comments
    
    Because:
     - More clear to read
     - Getting file ready to pull to main branch
    
     Split the code into 4 functions, changed the for loop and added comments
    
    * Changed for-loop and changed comments.
    
    Because:
     - For-loop chose wrong face.
     - Some comments where not finished.
    
     The for-loop didn't return right face and comments were not finished.
    
    * Changed file- and map name.
    
    Because:
     - I couldn't import the functions in the test file.
    
    Changed the "-" to "_" so I can import the functions in the test file.
    
    * Added test functions for face_detection.py.
    
    Because:
     - Every working python file needs a test file.
     - To see if all the functions are working correctly.
    
    Added test functions for face_detection.py and one to check raise error.
    
    * Flak8 run for this code.
    
    * Changed names and paths and removes test_placeholder.py.
    
    Because:
     - Its more clean to just put a file instead of a whole path.
     - No placeholder needed anymore.
    
    Removes "_" from file and folder name, changed dir for import file.
    
    * Ignore flake8 error.
    
    Because:
     - Otherwise the testfile will break.
    
    In setup.cfg I added E402 to the ignore.
    
    * Added PIL so the testfile doesn't need opencv.
    
    Because:
     - This might avoid the error when pulling to main.
    
    Instead of using opencv in testfile we use PIL and numpy to do the same.
    
    * Added Pillow to requirements.txt.
    
    Because:
     - The file test_face_detection.py needs PIL.
    
    Added Pillow to requirements.txt.
    
    * Made a .npy file of the image to ignore the workflow error.
    
    Because:
     - Needed to merge the code into main.
    
    * Made a .npy file of the image to ignore the workflow error.
    
    Because:
     - Needed to merge the code into main.
    
    
    Co-authored-by: Maria Dukmak <62339103+MariaDukmak@users.noreply.github.com>

---
+++ facedetection/face_detection.py
@@ -0,0 +1,124 @@
+"""Converts image to image with just a face in byte format."""
+import cv2
+import os
+import numpy as np
+
+# load classifier from file
+face_cascade = cv2.CascadeClassifier('facedetection/haarcascade_frontalface_default.xml')
+
+
+def crop_to_face(image: np.ndarray, face: np.ndarray) -> np.ndarray:
+    """
+    Crops image to just the face and reshapes the image to 48 x 48.
+
+    Parameters
+    ----------
+        image: np.ndarray
+            An image in numpy array format.
+
+        face: np.ndarray
+            Array that contains the point where te face starts and the width and height of the face.
+
+    Return
+    ------
+        frame_of_face
+            An image in numpy array format that is just the face.
+    """
+    print(os.getcwd())
+    (x, y, width, height) = face
+    frame_of_face = image[y: y + height, x: x + width]
+    return frame_of_face
+
+
+def reshape_image(image: np.ndarray) -> np.ndarray:
+    """
+    Reshapes the given image to a 48 x 48 pixels image.
+
+    Parameters
+    ----------
+        image: np.ndarray
+            An image that needs to be reshaped to 48 x 48 pixels.
+
+    Return
+    ------
+        reshaped_frame
+            The image reshaped to 48 x 48 pixels.
+    """
+    reshaped_frame = cv2.resize(image, (48, 48))
+
+    return reshaped_frame
+
+
+def convert_to_bytes(image: np.ndarray) -> bytes:
+    """
+    Convert the given image to bytes.
+
+    Parameters
+    ----------
+        image: np.ndarray
+            An image that needs to be converted to bytes.
+    Return
+    ------
+        byte_im
+            String of bytes containing the image.
+    """
+    # convert np.array to one-dimensional numpy array
+    _, im_buf_arr = cv2.imencode(".png", image)
+
+    # convert one-dimensional numpy array to bytes
+    byte_im = im_buf_arr.tobytes()
+    return byte_im
+
+
+def face_from_image(img_file: str) -> bytes:
+    """
+    Read image, detects faces, picks closest face then crops the image to just the face
+    reshapes the image and returns it as bytes.
+
+    Parameters
+    ----------
+        img_file: str
+            Path to image file.
+
+    Raises
+    ------
+        Raises error if no face can be detected.
+
+    Return
+    ------
+        byte_im
+            An image of a face in a 48 x 48 image in byte format.
+    """
+    # reads image
+    image = cv2.imread(img_file)
+
+    # make image gray
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # get coordinates of detected face with a scaleFactor of 1.2 and a minNeighbors of 5
+    detected_faces = face_cascade.detectMultiScale(gray, 1.2, 5)
+
+    # if no face is detected raise error
+    if len(detected_faces) == 0:
+        raise Exception("No face was detected in the image")
+
+    # gets closest face out of the scanned faces
+    closest_face = None
+
+    for face in detected_faces:
+        if closest_face is not None:
+            if face[-1] > closest_face[-1] and face[-2] > closest_face[-2]:
+                closest_face = face
+        else:
+            closest_face = face
+
+    # crops image to face
+    frame = crop_to_face(image, closest_face)
+
+    # reshapes image to 48 x 48
+    reshaped_frame = reshape_image(frame)
+
+    # convert image to string of bytes
+    byte_im = convert_to_bytes(reshaped_frame)
+
+    return byte_im
