1ab37cc745910b7a7d709841c94b4726e0b79433
AHipWhale <charlie.choffat@gmail.com>
2021-06-08 12:30:45 +0200

    Added PIL so the testfile doesn't need opencv.
    Because:
     - This might avoid the error when pulling to main.
    
    Instead of using opencv in testfile we use PIL and numpy to do the same.

--- tests/test_face_detection.py
+++ tests/test_face_detection.py
@@ -1,9 +1,9 @@
 """Tests for the face detection model."""
 import os
 
-import cv2
 import numpy as np
 import pytest
+from PIL import Image
 
 os.chdir("../facedetection/datafacedetection")
 from facedetection.face_detection import crop_to_face, reshape_image, convert_to_bytes, face_from_image
@@ -11,7 +11,8 @@ from facedetection.face_detection import crop_to_face, reshape_image, convert_to
 
 def test_crop_to_face():
     """Test the crop_to_face function by checking if the output is the right shape."""
-    image = cv2.imread("stormface.BMP")
+    img = Image.open("stormface.BMP")
+    image = np.asarray(img)
     width = 746
     height = 746
     face = np.array([432, 256, width, height])
@@ -21,14 +22,16 @@ def test_crop_to_face():
 
 def test_reshape_image():
     """Test the reshape_image function by checking if the output is of shape 48 x 48."""
-    image = cv2.imread("stormface.BMP")
+    img = Image.open("stormface.BMP")
+    image = np.asarray(img)
     output = reshape_image(image)
     assert np.shape(output) == (48, 48, 3)
 
 
 def test_convert_to_bytes():
     """Test the convert_to_bytes function by checking if the output type is bytes."""
-    image = cv2.imread("stormface.BMP")
+    img = Image.open("stormface.BMP")
+    image = np.asarray(img)
     output = convert_to_bytes(image)
     assert type(output) is bytes
 
@@ -37,7 +40,6 @@ def test_face_from_image():
     """Test face_from_image function by checking if the image runs all the way to the
        end without errors and checking if the output is type bytes.
     """
-    print(os.getcwd())
     output = face_from_image("stormface.BMP")
     assert type(output) is bytes
 
