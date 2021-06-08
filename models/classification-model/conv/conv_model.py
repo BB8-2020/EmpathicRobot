59d4b9e6f3ac3bcbfaf9f04db93f0725ffa21707
Maria Dukmak <62339103+MariaDukmak@users.noreply.github.com>
2021-06-08 22:56:07 +0200

    Split the run notebook into two. That makes the notebook readable and les complex. Needed  - to substantiate the models using both datasets

--- models/classification-model/conv/conv_model.py
+++ models/classification-model/conv/conv_model.py
@@ -1,4 +1,9 @@
+import _pickle as cPickle
+import bz2
 from typing import Tuple
+
+import tensorflow as tf
+from tensorflow import keras
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout, BatchNormalization
 
 
@@ -62,3 +67,47 @@ def build_models(input_shape: Tuple[int, int, int] = (48, 48, 1), num_classes: i
         ]
     }]
     return models_settings
+
+
+def read_data(path: str, datagen: bool = False):
+    if datagen:
+        datagen, x_train, y_train, x_val, y_val, x_test, y_test = cPickle.load(bz2.BZ2File(str(path), 'rb'))
+        return datagen, x_train, y_train, x_val, y_val, x_test, y_test
+
+    else:
+        x_train, y_train, x_val, y_val, x_test, y_test = cPickle.load(bz2.BZ2File(str(path), 'rb'))
+        return x_train,  y_train, x_val, y_val, x_test, y_test
+
+
+def fit_model(model: keras.Sequential, batch_size: int = 64, epochs: int = 100, dategen: bool = False, *data):
+
+    if dategen:
+        datagen, x_train, y_train, x_val, y_val, x_test= data
+        history = model.fit(datagen.flow(x_train, y_train, batch_size=batch_size),
+                            epochs=epochs,
+                            steps_per_epoch=len(x_train) // batch_size,
+                            validation_data=(x_val, y_val), verbose=2)
+    else:
+        x_train, y_train, x_val, y_val, x_test = data
+
+        history = model.fit(x_train, y_train, batch_size=batch_size,
+                            epochs=epochs,
+                            steps_per_epoch=len(x_train) // batch_size,
+                            validation_data=(x_val, y_val), verbose=2)
+    return history
+
+
+def compile_model(model: keras.Sequential):
+
+    model.compile(optimizer=tf.keras.optimizers.Adam(lr=0.0001),
+                  loss='binary_crossentropy',
+                  metrics=['accuracy'])
+
+
+def evaluate_model(model: keras.Sequential, x_test, y_test, batch_size: int = 64):
+    test_loss, test_acc = model.evaluate(x_test, y_test, batch_size=batch_size)
+    return test_loss, test_acc
+
+
+def main():
+    pass
