bae1993636b0f667cd6e3f63789524272e8f80f7
Maria Dukmak <62339103+MariaDukmak@users.noreply.github.com>
2021-06-10 11:14:53 +0200

    Deleted functions that were not used.

--- models/functions.py
+++ models/functions.py
@@ -38,30 +38,6 @@ def plot_acc_loss(history: tf.keras.callbacks) -> None:
     plt.show()
 
 
-def save_model_and_weights(model: tf.keras.Sequential, test_acc: float) -> None:
-    """
-    Save the model and the wights separate to a JSON file.
-    this could be used later to test and use the model.
-
-    Parameters
-    ----------
-        model: tf.keras.Sequential
-            the model that should be saved
-        test_acc: float
-            the results of the test dataset on the model, used to give the model unique name.
-    """
-    # Serialize and save model to JSON
-    test_acc = int(test_acc * 10000)
-    model_json = model.to_json()
-
-    with open('Saved-Models\\model' + str(test_acc) + '.json', 'w') as json_file:
-        json_file.write(model_json)
-
-    # Serialize and save weights to JSON
-    model.save_weights('Saved-Models\\model' + str(test_acc) + '.h5')
-    print('Model and weights are saved in separate files.')
-
-
 def save_all_model(model: tf.keras.Sequential, test_acc: float) -> None:
     """
     Save the whole model using .save from keras.
@@ -102,30 +78,3 @@ def save_model_to_lite(model: tf.keras.Sequential, test_acc: float) -> None:
     # Save the model
     with open(f'lite_model{test_acc}.tflite', 'wb') as f:
         f.write(tflite_model)
-
-
-def load_model_and_weights(model_path: str, weights_path: str) -> None:
-    """
-    Load a saved model form the directory and upload the weights to the model.
-
-    Parameters
-    ----------
-        model_path: str
-            string path to the saved model.
-        weights_path: str
-            string path to the saved wights.
-    """
-    # check if the file does exists
-    if os.path.exists(model_path) and os.path.exists(weights_path):
-        # Loading JSON model
-        json_file = open(model_path, 'r')
-        loaded_model_json = json_file.read()
-        json_file.close()
-        model = model_from_json(loaded_model_json)
-
-        # Loading weights
-        model.load_weights(weights_path)
-        model.compile(optimizer=tf.keras.optimizers.Adam, loss='binary_crossentropy', metrics=['accuracy'])
-        print('Model and weights are loaded and compiled.')
-    else:
-        raise OSError('File or directory does not exist!')
