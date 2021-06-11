8bd70e49fe926a39e147ba6df692c466d422b7d0
AHipWhale <charlie.choffat@gmail.com>
2021-06-11 14:49:05 +0200

    Changed tests and commented out 2 of them.
    Because:
     - It makes the tests more clean.
     - It would take too long to run the 2 tests.
    
    Changed tests so it doesn't repeat the same function.

--- tests/test_model_functions.py
+++ tests/test_model_functions.py
@@ -1,4 +1,5 @@
-"""Tests for model_functions.py."""
+"""Tests for model_functions.py. The fit_model function or tests that use that function are commented out of the code
+   because it would take too long to run, because the model gets fitted every time."""
 import pytest
 
 from tensorflow.keras import Sequential
@@ -8,42 +9,53 @@ from models.classification_model.model_functions import fit_model
 from models.classification_model.model_functions import compile_model
 from models.classification_model.model_functions import evaluate_model
 
-# every function needs this so we run it ones and use the output in the tests
-model = build_models()
-data = read_data("tests/dataclassificationmodel/ferPlus_processed.pbz2", False)
-seq_model = Sequential(model[0]['layers'], name=model[0]['name'])
-compile_model(seq_model)
-fitted_model = fit_model(seq_model, 64, 1, False, data[0], data[1], data[2], data[3], data[4])
+@pytest.fixture
+def model_data():
+    x_train, y_train, x_val, y_val, x_test, y_test = read_data("tests/dataclassificationmodel/ferPlus_processed.pbz2", False)
+    return x_train, y_train, x_val, y_val, x_test, y_test
 
-def test_read_data_processed():
+@pytest.fixture
+def sequential_model():
+    model = build_models()
+    seq_model = Sequential(model[0]['layers'], name=model[0]['name'])
+    return seq_model
+
+# @pytest.fixture
+# def fitted_model(model_data, sequential_model):
+#     x_train, y_train, x_val, y_val, x_test, _ = model_data
+#     compile_model(sequential_model)
+#     fitted_model = fit_model(sequential_model, 64, 1, False, x_train, y_train, x_val, y_val, x_test)
+#     return fitted_model
+
+
+def test_read_data_processed(model_data):
     """Testing the function by checking if the output is the correct size and type."""
-    global data
-    assert len(data) == 6 and type(data) is tuple
+    assert len(model_data) == 6 and type(model_data) is tuple
 
 
 def test_read_data_augmented():
     """Testing the function by checking if the output is the correct size and type."""
-    data = read_data("tests/dataclassificationmodel/ferPlus_augment.pbz2")
-    assert len(data) == 6 and type(data) is tuple
+    data = read_data("tests/dataclassificationmodel/ferPlus_augment.pbz2", True)
+    assert len(data) == 7 and type(data) is tuple
 
 
-def test_fit_model():
-    """"Testing the function by checking if the loss isn't 0."""
-    global fitted_model
-    assert fitted_model.history['loss'] != 0
+# def test_fit_model(fitted_model):
+#     """"Testing the function by checking if the loss isn't 0."""
+#     assert fitted_model.history['loss'] != 0
 
 
-def test_compile_model():
-    """Testing the compile_model function by checking if the fit_model function can be run without errors."""
-    global seq_model, data
-    try:
-        fit_model(seq_model, 64, 1, False, data[0], data[1], data[2], data[3], data[4])
-    except RuntimeError:
-        pytest.fail("Unexpected RuntimeError: Model needs to be compiled before fitting.")
+# def test_compile_model(sequential_model, model_data, fitted_model):
+#     """Testing the compile_model function by checking if the fit_model function can be run without errors."""
+#     x_train, y_train, x_val, y_val, x_test, _ = model_data
+#     try:
+#         fitted_model
+#     except RuntimeError:
+#         pytest.fail("Unexpected RuntimeError: Model needs to be compiled before fitting.")
 
 
-def test_evaluate_model():
+def test_evaluate_model(sequential_model, model_data):
     """Testing the evaluate_model function by checking the length of the output."""
-    global seq_model, data
-    output = evaluate_model(seq_model, data[4], data[5], 64)
+    _, _, _, _, x_test, y_test = model_data
+    compile_model(sequential_model)
+    output = evaluate_model(sequential_model, x_test, y_test, 64)
     assert len(output) == 2
