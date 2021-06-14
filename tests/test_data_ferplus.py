081a606afd6b8d1455de869d2ddd914b8f687f52
stormjoannes <stormjoannes10@hotmail.com>
2021-06-14 11:21:21 +0200

    FerPlus test functions done.
    Because:
    - Code testing

--- tests/test_data_ferplus.py
+++ tests/test_data_ferplus.py
@@ -3,6 +3,7 @@ import random
 import pytest
 import numpy as np
 import pandas as pd
+
 from data.ferPlus.ferPlus_functions import preprocess_data, clean_data_and_normalize
 
 
@@ -14,19 +15,30 @@ def test_preprocess_data():
     ------
         Check if the outcoming size of images equals the wanted shape.
     """
-    pixelstring = [random.randrange(0, 248) for i in range(2304)]
-    emotion = 'happiness'
+    pixelstring = ''
+    for i in range(2304):
+        x = random.randrange(0, 248)
+        pixelstring += f'{x} '
 
     data = {'pixels': [pixelstring],
-            'emotion': [emotion]
-            }
+            "happiness": [0],
+            "surprise": [0],
+            "sadness": [0],
+            "anger": [6],
+            "disgust": [1],
+            "fear": [3],
+            "contempt": [0],
+            "unknown": [0],
+            "NF": [0],
+             }
 
-    df = pd.DataFrame(data, columns=['pixels', 'emotion'])
+    df = pd.DataFrame(data, columns=['pixels', 'happiness', "neutral", 'surprise', "sadness", "anger", "disgust", "fear", "contempt", "unknown", "NF",])
 
-    preprocess_data(df, df)
-    size = np.array(df['pixels'][0]).shape
-    expected_size = (48, 48)
+    X, y = preprocess_data(df, df)
+    size = X[0].shape
+    expected_size = (48, 48, 1)
     assert size == expected_size
+
 
 def test_clean_data_and_normalize():
     """
@@ -34,11 +46,11 @@ def test_clean_data_and_normalize():
 
     Assert
     ------
-        Check if the target values are normalized and dummied.
+        Check if the target values are normalized and dummied to the right shapes.
     """
-    X = np.array([255, 255, 255, 255, 255, 255, 255])
-    y = np.array([0, 1, 2, 3, 4, 5, 6])
+    X = np.array([255.0, 255.0, 255.0, 255.0, 255.0, 255.0, 255.0])
+    y = np.array([9, 8, 7, 6, 5, 4, 3, 2, 1, 0])
 
     X, y = clean_data_and_normalize(X, y)
 
-    assert (int(X[0]), y[0].shape) == (1, (7, 7))
+    assert (int(X[0][0]), y[0].shape) == (1, (7,))
